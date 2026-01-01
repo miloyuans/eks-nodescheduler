6e8819ce3d904a9a943a28a50267f9e6d7811a05
milo <miloyuans@gmail.com>
2026-01-01 18:39:35 +0400

    up server good code

--- central/server/http_server.go
+++ central/server/http_server.go
@@ -2,22 +2,39 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
 
 	"central/config"
 	"central/middleware"
+	pb "path/to/proto"
 )
 
 func StartHTTP(wg *sync.WaitGroup, cfg *config.GlobalConfig, central *Central) {
 	defer wg.Done()
 
-	whitelist := middleware.NewWhitelist(cfg.Whitelist)
+	whitelist := middleware.New(cfg.Whitelist)
 	mux := http.NewServeMux()
-	mux.Handle("/report", whitelist.HTTPMiddleware(http.HandlerFunc(central.httpReportHandler)))
+	mux.Handle("/report", whitelist.HTTP(http.HandlerFunc(central.httpReportHandler)))
 
-	addr := cfg.Server.HTTP.Addr + ":" + fmt.Sprintf("%d", cfg.Server.HTTP.Port)
+	addr := fmt.Sprintf("%s:%d", cfg.Server.HTTP.Addr, cfg.Server.HTTP.Port)
 	log.Printf("HTTP server starting on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
+func (central *Central) httpReportHandler(w http.ResponseWriter, r *http.Request) {
+	var req pb.ReportRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	if ch, ok := central.clusterChans[req.ClusterName]; ok {
+		ch <- &req
+		w.Write([]byte(`{"success": true}`))
+	} else {
+		http.Error(w, "Cluster not found", http.StatusNotFound)
+	}
 }
