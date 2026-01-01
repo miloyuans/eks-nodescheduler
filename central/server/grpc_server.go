e7554534c16c836fd265e66ceb3b9431c2061e23
milo <miloyuans@gmail.com>
2026-01-01 18:39:51 +0400

    up good code

--- central/server/grpc_server.go
+++ central/server/grpc_server.go
@@ -2,25 +2,29 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"sync"
 
 	"central/config"
+	"central/middleware"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "path/to/proto"
 )
 
 func StartGRPC(wg *sync.WaitGroup, cfg *config.GlobalConfig, central *Central) {
 	defer wg.Done()
 
-	addr := cfg.Server.GRPC.Addr + ":" + fmt.Sprintf("%d", cfg.Server.GRPC.Port)
+	whitelist := middleware.New(cfg.Whitelist)
+	addr := fmt.Sprintf("%s:%d", cfg.Server.GRPC.Addr, cfg.Server.GRPC.Port)
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("gRPC listen failed: %v", err)
 	}
-
 	grpcServer := grpc.NewServer(
-		grpc.UnaryInterceptor(central.grpcWhitelistInterceptor),
+		grpc.UnaryInterceptor(whitelist.GRPCUnary),
 	)
 	pb.RegisterAutoscalerServiceServer(grpcServer, central)
 	reflection.Register(grpcServer)
