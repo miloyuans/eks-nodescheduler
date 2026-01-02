db2329e6c955940c453a69c390467e2e3b30143b
milo <miloyuans@gmail.com>
2026-01-02 20:35:08 +0400

    up good code new

--- central/processor/cluster_processor.go
+++ central/processor/cluster_processor.go
@@ -3,6 +3,7 @@ package processor
 
 import (
 	"context"
+	"crypto/md5"
 	"fmt"
 	"log"
 	"sync"
@@ -12,6 +13,7 @@ import (
 	"central/core"
 	"central/model"
 	"central/notifier"
+	"central/storage"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
